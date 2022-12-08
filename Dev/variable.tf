00f9c8c61b6b77fb3c568a3d9217ddb3022670c8
shivakpkp <shivakp.kp@gmail.com>
2022-12-08 21:13:27 +0000

    A lot commit

---
+++ Dev/variable.tf
@@ -0,0 +1,31 @@
+variable "cider_block" {
+  type = map(list(object({
+    item   = number,
+    zone   = string,
+    ip     = string,
+    public = bool
+  })))
+
+
+  default = {
+    sub = [
+      { item = 1, zone = "us-east-1b", ip = "10.100.0.0/19", public = true },
+      { item = 2, zone = "us-east-1b", ip = "10.100.32.0/19", public = false },
+      { item = 3, zone = "us-east-1c", ip = "10.100.64.0/19", public = true },
+      { item = 4, zone = "us-east-1c", ip = "10.100.96.0/19", public = false },
+      { item = 5, zone = "us-east-1d", ip = "10.100.128.0/19", public = true },
+      { item = 6, zone = "us-east-1d", ip = "10.100.160.0/19", public = false }
+  ] }
+}
+
+variable "cidr_block_range" {
+  default = "10.100.0.0/16"
+}
+
+variable "env"{
+    default = "dev"
+}
+
+variable "bucketname"{
+  default = "dev-terraformlovers"
+}
---
+++ Staging/variable.tf
@@ -0,0 +1,27 @@
+variable "cider_block" {
+  type = map(list(object({
+    item   = number,
+    zone   = string,
+    ip     = string,
+    public = bool
+  })))
+
+
+  default = {
+    sub = [
+      { item = 1, zone = "us-east-1b", ip = "10.200.0.0/19", public = true },
+      { item = 2, zone = "us-east-1b", ip = "10.200.32.0/19", public = false },
+      { item = 3, zone = "us-east-1c", ip = "10.200.64.0/19", public = true },
+      { item = 4, zone = "us-east-1c", ip = "10.200.96.0/19", public = false },
+      { item = 5, zone = "us-east-1d", ip = "10.200.128.0/19", public = true },
+      { item = 6, zone = "us-east-1d", ip = "10.200.160.0/19", public = false }
+  ] }
+}
+
+variable "cidr_block_range" {
+  default = "10.200.0.0/16"
+}
+
+variable "env"{
+    default = "dev"
+}
