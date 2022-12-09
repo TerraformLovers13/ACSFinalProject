fecadd76d6d465ca5fe34da9211592334a22dbc6
shivakpkp <96741085+shivakpkp@users.noreply.github.com>
2022-12-09 10:24:31 -0500

    changing in index.html

--- Modules/install_httpd.sh
+++ Modules/install_httpd.sh
@@ -1,43 +1,41 @@
 #!/bin/bash
-yum -y update
-yum -y install httpd
+sudo yum -y update
+sudo yum -y install httpd
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 scp /Modules/
 echo "<html>
   <head>
-<title>Flowers Webserver</title>
+<title>TerraformCI-CD</title>
  </head>
  <body>
 <center>
-    <h1>Created by terraformlavers</h1>
+    <h1>Created by TerraformLovers</h1>
 </center>
 
 <table border="5" bordercolor="grey" align="center">
     <tr>
-        <th colspan="3">My Favourite Flowers</th> 
+        <th colspan="3">Irina Geiman</th> 
     </tr>
     <tr>
-        <th>Spring</th>
-        <th>Summer</th>
-        <th>Fall</th>
+        <th>TerraformLovers</th>
+        <th>Our private IP is $myip</th>
+            
     </tr>
     <tr>
-        <td><img src="/images/daffodil.jpeg" alt="" border=3 height=200 width=300></img></th>
-        <td><img src="/images/rose.jpeg" alt="" border=3 height=200 width=300></img></th>
-        <td><img src="/images/hibiscus.jpeg" alt="" border=3 height=200 width=300></img></th>
+        <td><img src="daffodil.jpeg" alt="shiva" border=3 height=200 width=300></img></th>
+        <td><img src="rose.jpeg" alt="Carol" border=3 height=200 width=300></img></th>
+
     </tr>
     <tr>
-        <td>><img src="/images/tulip.jpeg" alt="" border=3 height=200 width=300></img></th>
-        <td><img src="/images/daisy.jpeg" alt="" border=3 height=200 width=300></img></th>
-        <td><img src="/images/sunflower.jpeg" alt="" border=3 height=200 width=300></img></th>
+        <td>><img src="hibiscus.jpeg" alt="Himanshu" border=3 height=200 width=300></img></th>
+        <td><img src="tulip.jpeg" alt="Philip" border=3 height=200 width=300></img></th>
+
     </tr>
 </table>
-
   </body>
   <html>
 "  >  /var/www/html/index.html
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo mkdir /var/www/images
-sudo aws s3 sync s3://${ver.env}-terraformlovers/images/ /var/www/images
-
+sudo aws s3 sync s3://${ver.env}-terraformlovers/images/ /var/www/html
