64ebf79d6d0d321af038b3a3c0a4e330cf3c2a2a
carolinanonato <csnonato@gmail.com>
2022-12-09 13:59:35 +0000

    some markdown in the readme file

--- Dev/config.tf
+++ Dev/config.tf
@@ -5,3 +5,13 @@ terraform {
     region = "us-east-1"                 // Region where bucket is created
   }
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      #version = "~> 3.27"
+    }
+  }
+  # required_version = "~> 1.1.5" # 1.1.5 or above and below 1.2.0
+}
